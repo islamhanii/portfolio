dbe0238ab332236ecb0cf65e36b5f5ec09c24ff8
islamhanii <islamhani433@gmail.com>
2026-01-13 22:16:41 +0200

    feat: add dashboard assets - add error pages

---
+++ resources/views/dashboard/layouts/head.blade.php
@@ -0,0 +1,19 @@
+<!-- Title -->
+<title> @yield('title') </title>
+<!-- Favicon -->
+<link rel="icon" href="{{ URL::asset('assets/dashboard/img/brand/favicon.png') }}" type="image/x-icon" />
+<!-- Icons css -->
+<link href="{{ URL::asset('assets/dashboard/css/icons.css') }}" rel="stylesheet">
+<!--  Custom Scroll bar-->
+<link href="{{ URL::asset('assets/dashboard/plugins/mscrollbar/jquery.mCustomScrollbar.css') }}" rel="stylesheet" />
+<!--  Sidebar css -->
+<link href="{{ URL::asset('assets/dashboard/plugins/sidebar/sidebar.css') }}" rel="stylesheet">
+<!-- Sidemenu css -->
+<link rel="stylesheet" href="{{ URL::asset('assets/dashboard/css-rtl/sidemenu.css') }}">
+@yield('css')
+<!--- Style css -->
+<link href="{{ URL::asset('assets/dashboard/css-rtl/style.css') }}" rel="stylesheet">
+<!--- Dark-mode css -->
+<link href="{{ URL::asset('assets/dashboard/css-rtl/style-dark.css') }}" rel="stylesheet">
+<!---Skinmodes css-->
+<link href="{{ URL::asset('assets/dashboard/css-rtl/skin-modes.css') }}" rel="stylesheet">
